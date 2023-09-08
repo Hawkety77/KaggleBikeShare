f3414c97fb5f6fde1ffc198402c14648d3dca69a
Ty Hawkes <hawkety77@gmail.com>
2023-09-08 13:47:25 -0600

    added R.R

---
+++ R.R
@@ -0,0 +1,7 @@
+## libraries
+library(tidyverse)
+
+## Read in the data
+bike_train <- read_csv('train.csv')
+bike_test <- read_csv('test.csv')
+sample_submission <- read_csv('sampleSubmission.csv')
