44f477c1fa08b1d0d573bfc62223d426737002b1
Ty Hawkes <hawkety77@gmail.com>
2023-09-11 13:59:29 -0600

    eda on bikeshare

--- BikeShareEDA.R
+++ BikeShareEDA.R
@@ -1,7 +1,49 @@
 ## libraries
 library(tidyverse)
+library(vroom)
+library(patchwork)
+library(DataExplorer)
+library(skimr)
 
 ## Read in the data
 bike_train <- read_csv('train.csv')
 bike_test <- read_csv('test.csv')
 sample_submission <- read_csv('sampleSubmission.csv')
+
+## View data
+view(bike_train)
+view(bike_test)
+
+## Check for missing values
+skim(bike_train)
+
+## Checking data types
+glimpse(bike_train)
+
+## Assign Factors
+bike_train_factored <- bike_train %>%
+  mutate(season = as.factor(season)) %>%
+  mutate(holiday = as.factor(holiday))
+  
+## Correlation plot
+plot1 <- plot_correlation(bike_train_factored)
+
+## Temperature scatter plot
+plot2 <- ggplot(data = bike_train_factored) +
+  geom_point(aes(temp, count), alpha = .1, color = 'blue')
+
+## Season plot
+plot3 <- ggplot(data = bike_train_factored) + 
+  geom_col(aes(season, count, color = season))
+
+## Weather Plot
+plot4 <- ggplot(data = bike_train_factored) + 
+  geom_boxplot(aes(weather, count, group = weather))
+
+## Design 4 panel plot
+
+(plot1 + plot2) / (plot3 + plot4)
+
+
+
+
--- R.R
+++
@@ -1,7 +0,0 @@
-## libraries
-library(tidyverse)
-
-## Read in the data
-bike_train <- read_csv('train.csv')
-bike_test <- read_csv('test.csv')
-sample_submission <- read_csv('sampleSubmission.csv')
